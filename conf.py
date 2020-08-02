e5c5c2c4cdcb128050427f85637f1bd4d3f8ebbf
Yves Chevallier <yves-chevallier@heig-vd.ch>
2020-08-02 18:09:56 +0200

    Fix header and footers

--- conf.py
+++ conf.py
@@ -67,14 +67,52 @@ latex_elements = {
     'fontpkg': '',
     'fncychap': '',
     'printindex': r'\footnotesize\raggedright\printindex',
-    'preamble': '''
-        % Disable the ugly colouring of titles. Why does Sphinx do this?
-        \\definecolor{TitleColor}{rgb}{0,0,0}
-        \\definecolor{InnerLinkColor}{rgb}{0,0,0}
+    'preamble': r'''
 
-        \usepackage{fontspec}
-        \setmonofont{DejaVu Sans Mono}
-    '''
+% Disable the ugly colouring of titles. Why does Sphinx do this?
+\definecolor{TitleColor}{rgb}{0,0,0}
+\definecolor{InnerLinkColor}{rgb}{0,0,0}
+
+\usepackage{fontspec}
+\setmonofont{DejaVu Sans Mono}
+
+\usepackage{colortbl}
+\usepackage{xcolor,graphicx}
+\usepackage[xparse,skins,breakable]{tcolorbox}
+
+\newtcolorbox{hint}{breakable,enhanced,arc=0mm,colback=lightgray!5,colframe=lightgray,leftrule=11mm,%
+height from=1.3cm to 16cm,%
+overlay={\node[anchor=north west,outer sep=1mm] at (frame.north west) {
+    \includegraphics[width=2em]{../../icons/hint.pdf}}; }}
+
+\renewenvironment{sphinxnote}[1]
+    {\begin{hint}{#1}}
+    {\end{hint}}
+
+% Change code-block style
+\colorlet{aaa}{lightgray}
+\colorlet{foobar}{lightgray!8}
+\sphinxsetup{%
+  VerbatimColor={named}{foobar},
+  verbatimwithframe=true,
+  VerbatimBorderColor={named}{aaa},
+  verbatimborder=0.3mm,
+  OuterLinkColor={rgb}{0.55,0.06,0.09}
+}
+
+% Define header and footers
+\makeatletter
+\fancypagestyle{normal}{
+  \fancyhf{}
+  \fancyfoot{}
+  \fancyhead{}
+  \fancyhead[LE,RO]{{\thepage}}
+  \fancyhead[CO]{{\@title}}
+  \fancyhead[CE]{{\nouppercase{\rightmark}}}
+  \renewcommand{\headrulewidth}{0.2pt}
+}
+\makeatother
+'''
 }
 
 latex_logo = '_artifacts/heig-vd.pdf'
