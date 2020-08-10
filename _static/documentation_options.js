a467bba041ce82407c40c68b673b65c6faa23443
yves-chevallier <52489316+yves-chevallier@users.noreply.github.com>
2020-08-10 21:47:31 +0000

    Deploying to gh-pages from  @ ddb67941de2a47688c133ef727583941c0de818d 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.2.7-40-g5c76761',
+    VERSION: 'v0.2.7-41-gddb6794',
     LANGUAGE: 'fr',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
