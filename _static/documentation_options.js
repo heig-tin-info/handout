df531ff2ecb9a2430fe79e889883899dcbb6e2b5
yves-chevallier <52489316+yves-chevallier@users.noreply.github.com>
2021-05-30 19:26:30 +0000

    Deploying to gh-pages from @ heig-tin-info/handout@ff14f861ea369b46deca678265e2e150b97a18db 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.3.0-21-g720137d',
+    VERSION: 'v0.3.0-22-gff14f86',
     LANGUAGE: 'fr',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
